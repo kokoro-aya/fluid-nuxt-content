59d743a4322db3050f5de9863dc04e00c405ddcc
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2024-04-12 01:02:00 +0200

    [scss] Remove tailwind dependencies because fluid used scss preprocessor. Installed sass for scss dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', "@nuxtjs/tailwindcss", "nuxt-icon"],
+  modules: ['@nuxt/content', "nuxt-icon"],
+  css: [],
   ssr: false,
   content: {
     experimental: {
