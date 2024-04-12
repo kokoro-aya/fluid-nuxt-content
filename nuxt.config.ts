f59165ca9fc590dd344b1472c39747ee1be55e66
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2024-04-12 13:15:03 +0200

    [scss] Included bootstrap@4.6.1 as dependency as the fluid theme did

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxt/content', "nuxt-icon"],
-  css: [],
+  css: ['~/assets/css/main.scss'],
   ssr: false,
   content: {
     experimental: {
