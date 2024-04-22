e3052bad5db6d2bcc5185d4daa22ae243555a1ca
kokoro-aya <35118912+kokoro-aya@users.noreply.github.com>
2024-04-22 23:29:17 +0200

    [views] Refactored page views, extracted to AsideSpacingContainer and extracted Footer from all views

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,37 +2,34 @@
   <main>
     <CommonHeader />
 
-    <ContentDoc v-slot="{ doc }">
-      <div class="container-fluid nopadding-x">
-        <div class="row nomargin-x">
-          <div class="side-col d-none d-lg-block col-lg-2" style="padding-top: 0px">
-          </div>
+    <AsideSpacingContainer>
+      <div class="side-col d-none d-lg-block col-lg-2" style="padding-top: 0px">
+      </div>
 
-          <div class="col-lg-8 nopadding-x-md">
-            <div class="container nopadding-x-md" id="board-ctn">
-              <div id="board">
-                <article class="post-content mx-auto">
-                  <h1 id="seo-header">{{doc.title}}</h1>
-                  <div class="markdown-body">
-                    <ContentRenderer :value="doc" />
-                  </div>
-                  <hr>
-                  <!-- TODO: implement comment section -->
-                </article>
-              </div>
+      <ContentDoc v-slot="{ doc }">
+        <div class="col-lg-8 nopadding-x-md">
+          <div class="container nopadding-x-md" id="board-ctn">
+            <div id="board">
+              <article class="post-content mx-auto">
+                <h1 id="seo-header">{{doc.title}}</h1>
+                <div class="markdown-body">
+                  <ContentRenderer :value="doc" />
+                </div>
+                <hr>
+                <!-- TODO: implement comment section -->
+              </article>
             </div>
-          </div>
-
-          <div class="side-col d-none d-lg-block col-lg-2" style="padding-top: 0px">
           </div>
         </div>
 
-      </div>
-
-    </ContentDoc>
+        <div class="side-col d-none d-lg-block col-lg-2" style="padding-top: 0px">
+        </div>
+      </ContentDoc>
+    </AsideSpacingContainer>
   </main>
 </template>
 <script setup lang="ts">
 import CommonHeader from "~/views/header/CommonHeader.vue";
+import AsideSpacingContainer from "~/views/AsideSpacingContainer.vue";
 
 </script>
---
+++ views/Footer.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer>
+    <div class="footer-inner">
+      <div class="footer-content">
+        <a href="https://content.nuxt.com/" target="_blank" rel="nofollow noopener">
+          <span>Nuxt Content</span>
+        </a>
+        <i class="iconfont icon-love">@</i>
+        <a href="https://github.com/kokoro-aya" target="_blank" rel="nofollow noopener">
+          <span>aqua</span>
+        </a>
+        inspired by
+        <a href="https://hexo.io" target="_blank" rel="nofollow noopener">
+          <span>Hexo</span>
+        </a>
+        <i class="iconfont icon-love">@</i>
+        <a href="https://github.com/fluid-dev/hexo-theme-fluid" target="_blank" rel="nofollow noopener">
+          <span>Fluid</span>
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
